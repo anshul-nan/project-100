bb0440325290a79a5f3f1aedeee11a5bc1aca8ba
anshul-nan <105433706+anshul-nan@users.noreply.github.com>
2022-10-24 17:43:12 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoXeIU-xohsOwn2Kw9PQM_k0PVdeMnrL0",
+    authDomain: "project100-9835c.firebaseapp.com",
+    databaseURL: "https://project100-9835c-default-rtdb.firebaseio.com",
+    projectId: "project100-9835c",
+    storageBucket: "project100-9835c.appspot.com",
+    messagingSenderId: "505074108293",
+    appId: "1:505074108293:web:30e70c9abc143f922fa6ec"
+  };
+
+  firebase.initializeApp(firebaseConfig);
